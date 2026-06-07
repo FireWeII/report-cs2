5150bc019681bef53ed9e92049e1f04c4e4150f3
lleW <sumersktt@gmail.com>
2026-06-07 14:01:17 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import keyboard
 import os
 
 
+btn_panico = 'f11' #caso queria trocar a tecla de emergencia é só substituir o F11 pela tecla desejada
+
 def emergencia():
     os._exit(0)
 
-keyboard.add_hotkey('f11', emergencia) #caso queria trocar a tecla de emergencia é só substituir o F11 pela tecla desejada
+keyboard.add_hotkey(btn_panico, emergencia) #caso queria trocar a tecla de emergencia é só substituir o botão na variavel btn_panico
 
 fudido = 0
-num_reports = 0
-num_reports = int(num_reports)
+
 
 def report(alvo):
     with open('report_players.txt', 'r', encoding='utf-8') as arquivo:
@@ -53,7 +54,7 @@ def abrir_mouse():
 def target():
     contador = 0
     fudido = int(selecao.get())
-    while contador <= num_reports:
+    while contador <= (int(selecao2.get()) - 1):
         report(fudido)
         contador +=1
 
@@ -61,16 +62,14 @@ def target():
 
 janela = ctk.CTk()
 janela.title('Adestrador de cadelas!')
-janela.geometry('450x150')
-janela.minsize(450,150)
+janela.geometry('500x200')
+janela.minsize(500,200)
 janela.resizable(False, False)
 
 
 
 tela1 = ctk.CTkFrame(janela)
 tela1.pack(fill ='both', expand= True)
-
-
 
 
 texto1 = ctk.CTkLabel(tela1, text='Selecione o alvo:')
@@ -90,6 +89,9 @@ btn_iniciar.grid(row=4, column=0, padx=5,pady=10, columnspan=2)
 
 btn_mouse = ctk.CTkButton(tela1, text='Configurar', command=abrir_mouse)
 btn_mouse.grid(row=4, column=3, padx=5,pady=10, columnspan=2)
+
+texto_tecla = ctk.CTkLabel(tela1, text=f'Para para a execução aperte {btn_panico.upper()}!')
+texto_tecla.grid(row=5, column=1, columnspan=2)
 
 
 janela.mainloop()
